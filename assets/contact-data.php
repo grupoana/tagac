8098613513ee926bc0f77b8acb746e2d05fa5898
ruzaanistudio <fernando.romay@ruzaani.com>
2024-02-17 13:52:51 -0600

    First commit

---
+++ assets/contact-data.php
@@ -0,0 +1,47 @@
+<?php
+/**************************************************************
+ * Datos de contacto en Español
+**************************************************************/
+
+$dataPhone['es'] = '8180207005';
+$dataPhoneStr['es'] = '81 8020 7005';
+$dataWhatsapp['es'] = '528180207005';
+$dataEmail['es'] = 'ventas@tagac.com.mx';
+
+$streetAddress['es'] = 'José Silvestre Aramberri 149';
+$addressLocality['es'] = 'Escobedo';
+$addressRegion['es'] = 'Nuevo León';
+$postalCode['es'] = '66058';
+$addressCountry['es'] = 'Mexico';
+$dataAddress['es'] = 'José Silvestre Aramberri 149, Ampliación Lázaro Cárdenas,<br>
+Lázaro Cárdenas, 66058 Cd Gral Escobedo, N.L.';
+$dataAddressFoot['es'] = $dataAddress['es'];
+
+$dataHours['es'] = '<strong>Lunes - Viernes:</strong> 8AM - 6PM <br>
+<strong>Sábado:</strong> 8AM - 2PM <br>
+<strong>Domingo:</strong> Cerrado';
+
+/**************************************************************
+ * Datos de contacto en Inglés
+**************************************************************/
+
+$dataPhone['en'] = '3466366633';
+$dataPhoneStr['en'] = '346 636 6633';
+$dataWhatsapp['en'] = '528180207005';
+$dataEmail['en'] = 'sales@tagac.com.mx';
+
+$streetAddress['en'] = 'José Silvestre Aramberri 149';
+$addressLocality['en'] = 'Escobedo';
+$addressRegion['en'] = 'Nuevo León';
+$postalCode['en'] = '66058';
+$addressCountry['en'] = 'Mexico';
+$dataAddress['en'] = '<b>Houston</b><br>6807 Wynnwood Ln, Houston, TX 77008</p>
+<p><b>Phar</b><br>9801 S Keystone Dr, Phar, TX 78577';
+$dataAddressFoot['en'] = '<b>Houston: </b><br>6807 Wynnwood Ln, TX 77008</p>
+<p><b>Phar: </b><br>9801 S Keystone Dr, TX 78577';
+
+$dataHours['en'] = '<strong>Monday - Friday:</strong> 8AM - 6PM <br>
+<strong>Saturday:</strong> 8AM - 2PM <br>
+<strong>Sunday:</strong> Closed';
+
+?>
