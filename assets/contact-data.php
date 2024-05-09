0ae06885aded1830b775e31012b56e9fda71d326
Fernando Romay <fernando.romay@ruzaani.com>
2024-05-09 15:10:48 -0600

    Cambiado número de contacto

--- assets/contact-data.php
+++ assets/contact-data.php
@@ -5,16 +5,16 @@
 
 $dataPhone['es'] = '8116578916';
 $dataPhoneStr['es'] = '81 1657 8916';
-$dataWhatsapp['es'] = 'https://wa.me/message/GKP63BFYVKUCG1';
+$dataWhatsapp['es'] = 'https://wa.me/message/RBL74RJ6Z7V3H1';
 $dataEmail['es'] = 'ventas@tagac.com.mx';
 
-$streetAddress['es'] = 'José Silvestre Aramberri 149';
-$addressLocality['es'] = 'Escobedo';
+$streetAddress['es'] = 'Código Agrario 5119';
+$addressLocality['es'] = 'Monterrey';
 $addressRegion['es'] = 'Nuevo León';
-$postalCode['es'] = '66058';
+$postalCode['es'] = '64108';
 $addressCountry['es'] = 'Mexico';
-$dataAddress['es'] = 'José Silvestre Aramberri 149, Ampliación Lázaro Cárdenas,<br>
-Lázaro Cárdenas, 66058 Cd Gral Escobedo, N.L.';
+$dataAddress['es'] = "{$streetAddress['es']},<br> Plutarco Elías Calles,<br>
+                      {$postalCode['es']} {$addressLocality['es']}, N.L.";
 $dataAddressFoot['es'] = $dataAddress['es'];
 
 $dataHours['es'] = '<strong>Lunes - Viernes:</strong> 8AM - 6PM <br>
@@ -30,7 +30,7 @@ $dataPhoneStr['en'] = '346 636 6633';
 $dataWhatsapp['en'] = '528180207005';
 $dataEmail['en'] = 'sales@tagac.com.mx';
 
-$streetAddress['en'] = 'José Silvestre Aramberri 149';
+$streetAddress['en'] = 'Código Agrario 5119';
 $addressLocality['en'] = 'Escobedo';
 $addressRegion['en'] = 'Nuevo León';
 $postalCode['en'] = '66058';
