e5a60e6e49e66a0a8f914fd78fa01371c6eb176a
Fernando Romay <fernando.romay@ruzaani.com>
2024-05-24 22:56:45 -0600

    Movido código reutilizable a sus propios archivos

--- index.php
+++ index.php
@@ -293,80 +293,14 @@ include "$basePath/assets/trans/index.php";
 
 
   <!--  Pie de Página  -->
-  <footer id="footer" class="footer">
+  <?php include "$basePath/assets/common/footer.php" ?>
 
-    <div class="container">
-      <div class="row gy-3">
-        <div class="col-lg-3 col-md-6 d-flex">
-          <i class="bi bi-geo-alt icon"></i>
-          <div>
-            <h2>
-              <?php echo $footAddress[$lang] ?>
-            </h2>
-            <p>
-              <?php echo $dataAddressFoot[$lang] ?><br>
-            </p>
-          </div>
-        </div>
+  <!-- Scripts comunes -->
+  <?php include "$basePath/assets/common/scripts.php" ?>
 
-        <div class="col-lg-3 col-md-6 footer-links d-flex">
-          <i class="bi bi-telephone icon"></i>
-          <div>
-            <h2>
-              <?php echo $footContact[$lang] ?>
-            </h2>
-            <p>
-              <strong>
-                <?php echo $footPhone[$lang] ?>
-              </strong> <a id="phone-footer" href="tel:<?php echo $dataPhone[$lang] ?>"><?php echo $dataPhoneStr[$lang] ?></a><br>
-              <strong>
-                <?php echo $footEmail[$lang] ?>
-              </strong> <a id="mail-footer" href="mailto:<?php echo $dataEmail[$lang] ?>"><?php echo $dataEmail[$lang] ?></a><br>
-            </p>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 footer-links d-flex">
-          <i class="bi bi-clock icon"></i>
-          <div>
-            <h2>
-              <?php echo $footHours[$lang] ?>
-            </h2>
-            <p>
-              <?php echo $dataHours[$lang] ?>
-            </p>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="copyright">
-        <?php echo $footRights[$lang] ?>
-      </div>
-
-    </div>
-
-  </footer>
-
-  <!-- Vendor JS Files -->
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-  <script src="https://www.tagac.com.mx/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="https://www.tagac.com.mx/assets/vendor/aos/aos.js"></script>
-  <script src="https://www.tagac.com.mx/assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="https://www.tagac.com.mx/assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="https://www.tagac.com.mx/assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="https://www.tagac.com.mx/assets/vendor/php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
+  <!-- Scripts de la página -->
   <script src="https://www.tagac.com.mx/assets/js/home.js"></script>
   <script src="https://www.tagac.com.mx/assets/js/cookies.js"></script>
-  <!--script src="assets/js/script.js"></script-->
-
-  <!-- Start of HubSpot Embed Code -->
-  <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/24005559.js"></script>
 
 </body>
 
